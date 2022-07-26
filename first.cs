a736fcb6284e7f8c38d43f452462a34a8a0efb86
Abdur-Rahman <69679506+ShafSpecs@users.noreply.github.com>
2022-07-26 16:02:46 +0100

    Create first.cs

--- first.cs
+++ first.cs
@@ -1,1 +1,24 @@
+using System;
+namespace ShafSpecs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double r, AREA;
+            const double PI = 3.14;   
  
+            Console.WriteLine("———————————————————————————————————————————");
+            Console.WriteLine("Program to calculate the area and circumference of a circle");
+            Console.WriteLine("———————————————————————————————————————————");
+
+            Console.Write("\nEnter the radius of circle ");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            AREA = PI * r * r;
+            Console.WriteLine("\nThe area of circle is {0} when radius is {1}", AREA, r);
+            Console.WriteLine("\nThe circumference of circle is {0}", 2 * PI * r);          
+            Console.WriteLine("———————————————————————————————————————————");
+        }
+    }
+} 
